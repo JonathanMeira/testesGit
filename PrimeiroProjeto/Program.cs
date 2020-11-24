5f694df09d64bf860a12841443e9508cb5d41cc5
JonathanMeira <jonathan_castilho_meira@hotmail.com>
2020-11-24 09:25:46 -0300

    Projeto iniciado utilizando foreach

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = null;
+            Console.WriteLine("Digite um nome: ");
+            x = Console.ReadLine();
+
+            Nome(x);
+
+
+        }
+
+        public static void Nome(string tst)
+        {
+            foreach (var item in tst)
+            {
+
+                if (item != ' ') { Console.WriteLine($"Esse é um {item}"); }
+
+            }
+        }
+
+    }
+
+
+}
+
